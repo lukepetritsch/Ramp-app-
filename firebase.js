196a8f5f6fb4e9179747ff326acfa3f24ee4ef1a
lukepetritsch <luke.petritsch2005@gmail.com>
2026-02-02 00:21:35 +0100

    Update firebase configuration with storage and measurement IDs

--- firebase.js
+++ firebase.js
@@ -1,6 +1,9 @@
- const firebaseConfig = {
-    apiKey: "AIzaSyCC9zB_me4qdyBmHZOTeUDP8IWoPNa6e1A",
-    authDomain: "ground-handling-log.firebaseapp.com",
-    projectId: "ground-handling-log",
-    appId: "1:771821879817:web:35ad14da4ab4613b832ed8"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCC9zB_me4qdyBmHZOTeUDP8IWoPNa6e1A",
+  authDomain: "ground-handling-log.firebaseapp.com",
+  projectId: "ground-handling-log",
+  storageBucket: "ground-handling-log.firebasestorage.app",
+  messagingSenderId: "771821879817",
+  appId: "1:771821879817:web:35ad14da4ab4613b832ed8",
+  measurementId: "G-SS17MF912N"
+};
