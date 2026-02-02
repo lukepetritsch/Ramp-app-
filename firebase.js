8432cc752399595919db2703e218116253cd8132
lukepetritsch <luke.petritsch2005@gmail.com>
2026-02-02 15:25:09 +0100

    Update firebase configuration and initialization

--- firebase.js
+++ firebase.js
@@ -1,15 +1,11 @@
 // firebase.js – Initialisierung
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCC9zB_me4qdyBmHZ0TeUDPB1WoPNa6e1A",
+apiKey: "AIzaSyCC9zB_me4qdyBmHZOTeUDP8IWoPNa6e1A",
   authDomain: "ground-handling-log.firebaseapp.com",
   projectId: "ground-handling-log",
-  appId: "1:771821879817:web:35ad14da4ab4613b832ed8"
+  storageBucket: "ground-handling-log.firebasestorage.app",
+  messagingSenderId: "771821879817",
+  appId: "1:771821879817:web:35ad14da4ab4613b832ed8",
+  measurementId: "G-SS17MF912N"
 };
-
-// Init
 firebase.initializeApp(firebaseConfig);
-
-// Globals
-const auth = firebase.auth();
-const db = firebase.firestore();
