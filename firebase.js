04dda27bef4014219da2d00276ca70afb1050dd8
lukepetritsch <luke.petritsch2005@gmail.com>
2026-02-02 15:32:24 +0100

    Remove Firebase initialization code

--- firebase.js
+++ firebase.js
@@ -1,11 +1,1 @@
-// firebase.js – Initialisierung
 
-apiKey: "AIzaSyCC9zB_me4qdyBmHZOTeUDP8IWoPNa6e1A",
-  authDomain: "ground-handling-log.firebaseapp.com",
-  projectId: "ground-handling-log",
-  storageBucket: "ground-handling-log.firebasestorage.app",
-  messagingSenderId: "771821879817",
-  appId: "1:771821879817:web:35ad14da4ab4613b832ed8",
-  measurementId: "G-SS17MF912N"
-};
-firebase.initializeApp(firebaseConfig);
